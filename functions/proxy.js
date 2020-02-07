47b1f444da9d666458f29636e9c92428572bbbcf
Jed Fox <git@twopointzero.us>
2020-02-07 10:53:26 -0500

    Update proxy.js

--- functions/proxy.js
+++ functions/proxy.js
@@ -1,3 +1,32 @@
-exports.handler = function(event, context, callback) {
-    // your server-side functionality
+const qs = require('querystring')
+const { promisify } = require('util')
+const request = promisify(require('https').get)
+
+exports.handler = async function(event, context) {
+  const { path } = event.queryStringParameters
+  const res = await request(`https://j_f.keybase.pub/${path}`, { method: event.httpMethod })
+
+  const data = await new Promise((resolve, reject) => {
+    const chunks = []
+    let len = 0
+    res.on('data', (chunk) => {
+      chunks.push(chunk)
+      len += chunk.length
+    })
+    res.on('error', reject)
+    res.on('end', () => {
+      resolve(Buffer.concat(chunks, len))
+    })
+  })
+
+  return {
+    statusCode: res.statusCode,
+    headers: {
+      ...res.headers,
+      'Access-Control-Allow-Origin': event.headers.Origin,
+      'Vary': (res.headers.vary || '').split(/,\s*/).concat('Origin').join(', '),
+    },
+    isBase64Encoded: true,
+    body: data.toString('base64')
+  }
 }
