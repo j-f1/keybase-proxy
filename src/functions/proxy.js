bbdd72966c7c305d2fc5fd0adab2f2281694ac85
Jed Fox <git@twopointzero.us>
2020-02-07 11:14:17 -0500

    Update proxy.js

--- src/functions/proxy.js
+++ src/functions/proxy.js
@@ -4,7 +4,7 @@ exports.handler = function(event, context, callback) {
   const { path } = event.queryStringParameters
   console.log('request for', path)
 
-  return callback({ statusCode: 200, isBase64Encoded: false, body: path })
+  return callback(null, { statusCode: 200, isBase64Encoded: false, body: path })
 
   request(`https://j_f.keybase.pub/${path}`, { method: event.httpMethod }, res => {
     console.log('got res')
